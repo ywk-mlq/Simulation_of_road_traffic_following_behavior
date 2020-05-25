b0e868108b9e6df49cd1b316ecaf196ce6e1f47a
wenkai-Yu <2019905327@chd.edu.cn>
2020-05-25 23:41:48 +0800

    Update main.cpp

--- Simulation_of_road_traffic_following_behavior/main.cpp
+++ Simulation_of_road_traffic_following_behavior/main.cpp
@@ -1,1 +1,24 @@
+/*************************************************
 
+Copyright:于文凯 马丽千
+
+Author:于文凯 马丽千
+
+Date:2020-05-24
+
+Description:主函数
+
+**************************************************/
+
+#include "Interface_management.h"
+
+/**
+*  开始程序
+*/
+int main()
+{
+	// 开始程序
+	Intrt start;
+	//start.startInter();
+	return 0;
+}
