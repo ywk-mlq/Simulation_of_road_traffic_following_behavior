9709a46330dd44b9b5657f258416e1f00acba18f
wenkai-Yu <2019905327@chd.edu.cn>
2020-05-30 13:48:28 +0800

    Update main.cpp

--- Simulation_of_road_traffic_following_behavior/main.cpp
+++ Simulation_of_road_traffic_following_behavior/main.cpp
@@ -10,6 +10,7 @@ Description:主函数
 
 **************************************************/
 
+#include <windows.h>
 #include "Interface_management.h"
 
 /**
@@ -17,6 +18,16 @@ Description:主函数
 */
 int main()
 {
+    // 调整界面
+    HWND hwnd = GetForegroundWindow();
+    int cx = GetSystemMetrics(SM_CXSCREEN); /* 屏幕宽度 像素 */
+    int cy = GetSystemMetrics(SM_CYSCREEN); /* 屏幕高度 像素 */
+    LONG l_WinStyle = GetWindowLong(hwnd, GWL_STYLE); /* 获取窗口信息 */
+    /* 设置窗口信息 最大化 取消标题栏及边框 */
+    SetWindowLong(hwnd, GWL_STYLE, (l_WinStyle | WS_POPUP | WS_MAXIMIZE)
+        & ~WS_CAPTION & ~WS_THICKFRAME & ~WS_BORDER);
+    SetWindowPos(hwnd, HWND_TOP, 0, 0, cx, cy, 0);
+
 	// 开始程序
 	Intrt start;
 	return 0;
